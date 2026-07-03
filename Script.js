51f63197d1eff20f1bd927afc2bd2de5f14a77bd
mikyas1299-glitch <mikyas1299@gmail.com>
2026-07-03 19:55:47 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,49 @@
+const text = "";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,100);
+
+}
+
+}
+
+typing();
+
+const sections =
+document.querySelectorAll("section");
+
+window.addEventListener("scroll",()=>{
+
+sections.forEach(section=>{
+
+const top=
+section.getBoundingClientRect().top;
+
+if(top < window.innerHeight-100){
+
+section.classList.add("show");
+
+}
+
+});
+
+});
+
+function openPortfolio(){
+    document.getElementById
+      ("welcome-screen").
+      style.display="none";
+    document.getElementById
+      ("portfolio-content").
+      style.display="block";
+}
 
